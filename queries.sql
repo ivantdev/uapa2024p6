ea52fbba72344067b9abe3361f836e4dba498786
Ivan Castellanos <ivan@ivant.dev>
2025-04-04 16:29:05 -0500

    Update container name in docker-compose and enhance SQL queries and schema for university database

--- queries.sql
+++ queries.sql
@@ -1,56 +1,111 @@
--- 1. ¿Cuántos docentes activos hay en la Facultad de Ingeniería?
+-- Diseñe una base de datos relacional (de preferencia en MySQL o MariaDB) para la Facultad de Ingeniería, la cual permita dar respuesta a las siguientes preguntas:
 
-SELECT COUNT(*) AS active_teachers
-FROM Teacher T
-JOIN Department D ON T.id_department = D.id_department
-JOIN Faculty F ON D.id_faculty = F.id_faculty
-WHERE T.teacher_status = 'ACTIVE' AND F.name = 'Facultad de Ingeniería';
+-- Para ejecutar directamente el código SQL:
+-- reemplazar ? con los datos reales
 
+-- 1. ¿Cuántos estudiantes activos hay en la Facultad de Ingeniería?
+SELECT COUNT(*) AS active_students_at_the_faculty_of_engineering
+FROM StudentPlan StP
+JOIN StudyPlan SP ON StP.id_plan = SP.id_plan
+JOIN Faculty F ON SP.id_faculty = F.id_faculty
+WHERE StP.is_active AND F.name = "Facultad de Ingeniería"; -- asegurese de coincidir con el registro en la db
 
+-- 2. ¿Cuáles son los cursos que cursaron en un periodo dado?
+-- asumiendo que un curso es considerado cursado si cuenta con mínimo un estudiante inscrito.
+SELECT C.*
+FROM Enrollment E
+JOIN CourseOffer CO ON CO.id_course_offer = E.id_course_offer
+JOIN Course C ON CO.id_course = C.id_course
+JOIN AcademicPeriod P ON CO.id_period = P.id_period
+WHERE P.year = ? AND P.semester = ?; -- reemplazar `?` para hacer una consutla directa con SQL
 
--- 2. ¿Cuáles son los cursos que se dictaron en un periodo dado?
-
-SELECT C.code AS course_code, C.name AS course_name, P.year, P.semester, T.first_name, T.last_name
-FROM CourseOffer AS CO
-JOIN Course AS C ON CO.id_course = C.id_course
-JOIN Period AS P ON CO.id_period = P.id_period
-JOIN Teacher AS T ON CO.id_teacher = T.id_teacher
-WHERE P.year = 2024 AND P.semester = '1'; -- Valores brutos (año y semestre)
-
-
-
--- 3. ¿Cuál es la lista de estudiantes para el periodo actual?
-
-SELECT S.first_name, S.last_name, S.type_document, S.document_number, S.username, S.phone, S.address
-FROM Student S
-JOIN Enrollment E ON S.id_student = E.id_student
-JOIN CourseOffer CO ON E.id_course_offer = CO.id_course_offer
-JOIN Period P ON CO.id_period = P.id_period
-WHERE P.year = YEAR(CURRENT_DATE())
-    AND P.semester = CASE 
-        WHEN MONTH(CURDATE()) BETWEEN 1 AND 6 THEN '1' 
-        ELSE '2' 
-    END; -- asumiendo semesetres normales
-
-
-
--- 4. ¿Cuál es el promedio de las calificaciones obtenidas en un curso los últimos 5 años?
-
+-- 3. ¿Cuál es el promedio de las calificaciones obtenidas en un curso los últimos 5 años?
+-- Se usa `... - 4` porque el rango quedaría en 6 años, incluyendo el acutal.
 SELECT C.name AS course_name, AVG(SG.grade) AS average_grade
 FROM StudentGrade SG
-JOIN Enrollment E ON SG.id_enrollment = E.id_enrollment
-JOIN CourseOffer CO ON E.id_course_offer = CO.id_course_offer
-JOIN Course C ON CO.id_course = C.id_course
-JOIN Period P ON CO.id_period = P.id_period
-WHERE P.year BETWEEN YEAR(CURRENT_DATE()) - 5 AND YEAR(CURRENT_DATE())
-    AND C.name = 'Cálculo Diferencial'; -- Valores burtos (nombre del curso)
+JOIN Enrollment E ON E.id_enrollment = SG.id_enrollment
+JOIN CourseOffer CO ON CO.id_course_offer = E.id_course_offer
+JOIN Course C ON C.id_course = CO.id_course
+JOIN AcademicPeriod P ON P.id_period = CO.id_period
+WHERE P.year BETWEEN YEAR(CURRENT_DATE()) - 4 AND YEAR(CURRENT_DATE())
+    AND C.code = ?; -- reemplazar `?` para hacer una consutla directa con SQL
 
+-- 4. ¿Cuáles son las asignaturas de mayor reprobación?
+SELECT C.code AS course_code, C.name AS course_name, COUNT(*) as total_failures
+FROM StudentGrade SG
+JOIN Enrollment E ON E.id_enrollment = SG.id_enrollment
+JOIN CourseOffer CO ON CO.id_course_offer = E.id_course_offer
+JOIN Course C ON C.id_course = CO.id_course
+WHERE SG.grade_status = "FAILED"
+GROUP BY course_code, course_name
+ORDER BY total_failures DESC;
 
+-- haciendolo por porcentaje:
+SELECT 
+    C.code AS course_code,
+    C.name AS course_name,
+    SUM(SG.grade_status = 'FAILED') AS total_failures,
+    COUNT(*) AS total_enrollments,
+    ROUND(SUM(SG.grade_status = 'FAILED') / COUNT(*) * 100, 2) AS failure_rate
+FROM StudentGrade SG
+JOIN Enrollment E ON E.id_enrollment = SG.id_enrollment
+JOIN CourseOffer CO ON CO.id_course_offer = E.id_course_offer
+JOIN Course C ON C.id_course = CO.id_course
+GROUP BY course_code, course_name
+ORDER BY failure_rate DESC;
 
--- 5. ¿Cuál sería el procedimiento para crear un nuevo curso y asignarle un docente?
+-- 5. ¿Cuál sería el procedimiento para ingresar los estudiantes de primera matrícula o de traslado?
+-- 5.1 Registrar al estudiante:
+INSERT INTO Student(first_name, last_name, type_document, document_number, username, phone, address)
+VALUES(?, ?, ?, ?, ?, ?, ?); -- reemplazar `?` para hacer una consutla directa con SQL
 
-INSERT INTO Course (name, code, description, credits, id_department)
-VALUES ('Cálculo Diferencial', '10000005', 'Curso de cálculo diferencial', 4, 1); -- Valores brutos
+-- 5.2a Registrar plan de estudios de primera matrícula
+INSERT INTO StudentPlan(id_student, id_plan, admission_type, admission_date)
+VALUES (?, ?, "NEW", ?); -- reemplazar `?` para hacer una consutla directa con SQL
 
-INSERT INTO CourseOffer (id_course, id_teacher, id_period)
-VALUES (2, 1, 1); -- Valores brutos
+-- 5.2b0 Traslado de un estudiante. Si su plan de estudios anterior esta activo, este debe ser bloqueado.
+--       Considerando que cada sede (campus) tiene su propia autonomía, considero que la mejor forma es
+--       blquear cada plan de estudios manualmente, en lugar de hacer todo en una sentecia SQL con Triggers.
+
+-- 5.2b1 Obtener el plan actual activo (normal o doble titulación, según el caso)
+SELECT id_student_plan
+FROM StudentPlan
+WHERE id_student = ? -- reemplazar `?` para hacer una consutla directa con SQL
+    AND is_active = TRUE
+    AND is_double_degree = FALSE;
+
+-- 5.2b2 Bloquear el plan de estudio y registrar la razón de bloqueo
+UPDATE StudentPlan SET is_active = FALSE WHERE id_student_plan = ?; -- reemplazar `?` para hacer una consutla directa con SQL
+
+INSERT INTO StudentPlanBlock(id_student_plan, id_block_plan_reason, notes)
+VALUES (?, ?, 'Bloqueo por traslado de sede'); -- reemplazar `?` para hacer una consutla directa con SQL
+
+-- 5.2b3 Registrar el plan de estudio al que se traslada el estudiante
+INSERT INTO StudentPlan(id_student, id_plan, admission_type, admission_date, transfer_date)
+VALUES (?, ?, 'TRANSFER', ?, ?); -- reemplazar `?` para hacer una consutla directa con SQL
+
+-- opcional: registrar el log del traslado
+INSERT INTO StudentTransferLog(id_student, from_plan, to_plan, transfer_date, notes)
+VALUES (?, ?, ?, ?, 'Traslado aprobado por consejo académica'); -- reemplazar `?` para hacer una consutla directa con SQL
+
+-- 5.2b4 Homologación de cursos
+
+-- Recuperar los cursos aprobados por el estudiante
+SELECT E.id_enrollment, C.name AS course_name, SG.grade, SG.grade_status
+FROM Enrollment E
+JOIN CourseOffer CO ON e.id_course_offer = CO.id_course_offer
+JOIN Course C ON CO.id_course = c.id_course
+JOIN StudentGrade SG ON SG.id_enrollment = e.id_enrollment
+WHERE e.id_student = ? -- reemplazar `?` para hacer una consutla directa con SQL
+    AND SG.grade_status = 'PASSED';
+
+-- Por cada curos aprobado hacer la respectiva homologación (si es posible)
+INSERT INTO CourseHomologation(
+    id_student,
+    id_enrollment,        -- del curso anterior
+    id_course_target,     -- del curso en el nuevo plan
+    id_old_plan,
+    id_new_plan,
+    homologation_date,
+    notes
+) VALUES (?, ?, ?, ?, ?, CURRENT_DATE, 'Homologación validada por el consejo académico'); -- reemplazar `?` para hacer una consutla directa con SQL
--- schema.sql
+++ schema.sql
@@ -1,6 +1,15 @@
+CREATE TABLE Campus (
+    id_campus INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(100),
+    UNIQUE (name)
+);
+
 CREATE TABLE Faculty (
     id_faculty INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    id_campus INT NOT NULL,
+    FOREIGN KEY (id_campus) REFERENCES Campus(id_campus)
 );
 
 CREATE TABLE Department (
@@ -28,7 +37,7 @@ CREATE TABLE Teacher (
     UNIQUE (username)
 );
 
-CREATE TABLE Period (
+CREATE TABLE AcademicPeriod (
     id_period INT AUTO_INCREMENT PRIMARY KEY,
     year INT NOT NULL,
     semester ENUM('1', '2') NOT NULL,
@@ -46,7 +55,8 @@ CREATE TABLE Course (
     description TEXT NOT NULL,
     credits INT NOT NULL,
     id_department INT NOT NULL,
-    FOREIGN KEY (id_department) REFERENCES Department(id_department)
+    FOREIGN KEY (id_department) REFERENCES Department(id_department),
+    UNIQUE (code)
 );
 
 CREATE TABLE CourseOffer (
@@ -58,7 +68,7 @@ CREATE TABLE CourseOffer (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_course) REFERENCES Course(id_course),
     FOREIGN KEY (id_teacher) REFERENCES Teacher(id_teacher),
-    FOREIGN KEY (id_period) REFERENCES Period(id_period)
+    FOREIGN KEY (id_period) REFERENCES AcademicPeriod(id_period)
     -- UNIQUE (id_course, id_teacher, id_period) -- el docente puede dictar un mismo curso en el mismo periodo más de una vez? por ahora si
 );
 
@@ -73,12 +83,41 @@ CREATE TABLE CourseSchedule (
     FOREIGN KEY (id_course_offer) REFERENCES CourseOffer(id_course_offer)
 );
 
+CREATE TABLE StudyPlan (
+    id_plan INT AUTO_INCREMENT PRIMARY KEY,
+    id_campus INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    code VARCHAR(50) NOT NULL UNIQUE,
+    total_credits INT DEFAULT 0 NOT NULL,
+    id_faculty INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_faculty) REFERENCES Faculty(id_faculty),
+    FOREIGN KEY (id_campus) REFERENCES Campus(id_campus)
+);
+
+CREATE TABLE StudyPlanCourse (
+    id_plan_course INT AUTO_INCREMENT PRIMARY KEY,
+    id_plan INT NOT NULL,
+    id_course INT NOT NULL,
+    semester TINYINT,
+    course_type ENUM('MANDATORY', 'ELECTIVE') NOT NULL DEFAULT 'MANDATORY',
+    FOREIGN KEY (id_plan) REFERENCES StudyPlan(id_plan),
+    FOREIGN KEY (id_course) REFERENCES Course(id_course),
+    UNIQUE (id_plan, id_course)
+);
+
+CREATE TABLE BlockPlanReason (
+    id_block_plan_reason INT AUTO_INCREMENT PRIMARY KEY,
+    reason_name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT
+);
+
 CREATE TABLE Student (
     id_student INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     type_document ENUM('CC', 'CE', 'PASSPORT') NOT NULL,
-    student_status ENUM('ACTIVE', 'INACTIVE') NOT NULL,
     document_number VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
     phone VARCHAR(50),
@@ -87,6 +126,49 @@ CREATE TABLE Student (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     UNIQUE (document_number),
     UNIQUE (username)
+);
+
+CREATE TABLE StudentPlan (
+    id_student_plan INT AUTO_INCREMENT PRIMARY KEY,
+    id_student INT NOT NULL,
+    id_plan INT NOT NULL,
+    total_used_credits INT DEFAULT 0 NOT NULL,
+    total_available_credits INT DEFAULT 0 NOT NULL,
+    admission_type ENUM('NEW', 'TRANSFER') NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    is_double_degree BOOLEAN NOT NULL DEFAULT FALSE,
+    admission_date DATE NOT NULL,
+    transfer_date DATE NULL,
+    FOREIGN KEY (id_student) REFERENCES Student(id_student),
+    FOREIGN KEY (id_plan) REFERENCES StudyPlan(id_plan),
+    CHECK (
+        (admission_type = 'NEW' AND transfer_date IS NULL) OR
+        (admission_type = 'TRANSFER' AND transfer_date IS NOT NULL)
+    )
+);
+
+CREATE TABLE StudentTransferLog (
+    id_transfer INT AUTO_INCREMENT PRIMARY KEY,
+    id_student INT NOT NULL,
+    from_plan INT NOT NULL,
+    to_plan INT NOT NULL,
+    transfer_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    notes TEXT,
+    FOREIGN KEY (id_student) REFERENCES Student(id_student),
+    FOREIGN KEY (from_plan) REFERENCES StudyPlan(id_plan),
+    FOREIGN KEY (to_plan) REFERENCES StudyPlan(id_plan)
+);
+
+CREATE TABLE StudentPlanBlock (
+    id_block INT AUTO_INCREMENT PRIMARY KEY,
+    id_student_plan INT NOT NULL,
+    id_block_plan_reason INT NOT NULL,
+    block_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    unblock_date DATE,
+    is_active BOOLEAN DEFAULT TRUE,
+    notes TEXT,
+    FOREIGN KEY (id_student_plan) REFERENCES StudentPlan(id_student_plan),
+    FOREIGN KEY (id_block_plan_reason) REFERENCES BlockPlanReason(id_block_plan_reason)
 );
 
 CREATE TABLE Enrollment (
@@ -104,6 +186,7 @@ CREATE TABLE StudentGrade (
     id_grade INT AUTO_INCREMENT PRIMARY KEY,
     id_enrollment INT NOT NULL,
     grade DECIMAL(3, 2) NOT NULL CHECK (grade >= 0 AND grade <= 5), -- por restricción de MariaDB se usa (3, 2) en lugar de (1, 2); DECIMAL(M, D) M > D
+    grade_status ENUM('PASSED', 'FAILED') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_enrollment) REFERENCES Enrollment(id_enrollment),
@@ -120,3 +203,88 @@ CREATE TABLE TeacherEvaluation (
     FOREIGN KEY (id_enrollment) REFERENCES Enrollment(id_enrollment),
     UNIQUE (id_enrollment)
 );
+
+CREATE TABLE CourseHomologation (
+    id_homologation INT AUTO_INCREMENT PRIMARY KEY,
+    id_student INT NOT NULL,
+    id_enrollment INT NOT NULL,
+    id_course_target INT NOT NULL,
+    id_old_plan INT NOT NULL,
+    id_new_plan INT NOT NULL,
+    homologation_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    notes TEXT,
+    FOREIGN KEY (id_student) REFERENCES Student(id_student),
+    FOREIGN KEY (id_enrollment) REFERENCES Enrollment(id_enrollment),
+    FOREIGN KEY (id_course_target) REFERENCES Course(id_course),
+    FOREIGN KEY (id_old_plan) REFERENCES StudyPlan(id_plan),
+    FOREIGN KEY (id_new_plan) REFERENCES StudyPlan(id_plan)
+);
+
+-- Verificar que exista solo un único plan de estudios activo normal y uno como doble titulación
+-- al momento de realizar una inserción
+DELIMITER $$
+
+CREATE TRIGGER check_unique_active_plans
+BEFORE INSERT ON StudentPlan
+FOR EACH ROW
+BEGIN
+    DECLARE active_plans_count INT;
+
+    SELECT COUNT(*) INTO active_plans_count
+    FROM StudentPlan
+    WHERE id_student = NEW.id_student
+      AND is_active = TRUE
+      AND is_double_degree = NEW.is_double_degree;
+
+    IF active_plans_count >= 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El estudiante ya tiene un plan activo de este tipo (normal o doble titulación).';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Verificar que exista solo un único plan de estudios activo normal y uno como doble titulación
+-- al momento de hacer una actualización en los planes de estudio de los estudiantes
+DELIMITER $$
+
+CREATE TRIGGER check_unique_active_plans_update
+BEFORE UPDATE ON StudentPlan
+FOR EACH ROW
+BEGIN
+    DECLARE active_plans_count INT;
+
+    IF NEW.is_active = TRUE THEN
+        SELECT COUNT(*) INTO active_plans_count
+        FROM StudentPlan
+        WHERE id_student = NEW.id_student
+          AND is_active = TRUE
+          AND is_double_degree = NEW.is_double_degree
+          AND id_student_plan != NEW.id_student_plan;
+
+        IF active_plans_count >= 1 THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'El estudiante ya tiene un plan activo de este tipo (normal o doble titulación).';
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Trigger para asignar los créditos disponibles del plan de estudios del estudiante, según StudyPlan
+DELIMITER $$
+
+CREATE TRIGGER set_available_credits_before_insert
+BEFORE INSERT ON StudentPlan
+FOR EACH ROW
+BEGIN
+    DECLARE plan_credits INT;
+
+    SELECT total_credits INTO plan_credits
+    FROM StudyPlan
+    WHERE id_plan = NEW.id_plan;
+
+    SET NEW.total_available_credits = plan_credits;
+END$$
+
+DELIMITER ;
